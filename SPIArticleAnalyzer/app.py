aee8ccf2f8eebef2687ebc8df7a8678c52574d4e
tools.spiarticleanalyzer <tools.spiarticleanalyzer@tools-bastion-03.tools.eqiad.wmflabs>
2017-02-08 04:03:45 +0000

    Updating files after successful Flask debugging.

--- SPIArticleAnalyzer/app.py
+++ SPIArticleAnalyzer/app.py
@@ -1,46 +1,98 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Tool Labs Flask + OAuth WSGI tutorial
+#
+# Copyright (C) 2017 Bryan Davis and contributors
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import flask
+import mwoauth
 import os
-from flask import Flask
-from flask_mwoauth import MWOAuth
-from builtins import input
+import yaml
 
-import sys
-import os
-from getAllUsers import *
 
-# Generate a random secret application key
-#
-# NOTE: this key changes every invocation. In an actual application, the key
-# should not change! Otherwise you might get a different secret key for
-# different requests, which means you can't read data stored in cookies,
-# which in turn breaks OAuth.
-#
-# So, for an actual application, use app.secret_key = "some long secret key"
-# (which you could generate using os.urandom(24))
-#
+app = flask.Flask(__name__)
 
-f = open("oauth.txt", "r")
 
-app.secret_key = f.readline().strip()
+# Load configuration from YAML file
+__dir__ = os.path.dirname(__file__)
+app.config.update(
+    yaml.load(open(os.path.join(__dir__, 'config.yaml'))))
 
-print("""
-NOTE: The callback URL you entered when proposing an OAuth consumer
-probably did not match the URL under which you are running this development
-server. Your redirect back will therefore fail -- please adapt the URL in
-your address bar to http://localhost:5000/oauth-callback?oauth_verifier=...etc
-""")
 
-consumer_key = f.readline().strip()
-consumer_secret = f.readline().strip()
-
-mwoauth = MWOAuth(consumer_key=consumer_key, consumer_secret=consumer_secret)
-app.register_blueprint(mwoauth.bp)
-
-@app.route("/")
+@app.route('/')
 def index():
-    return "logged in as: " + repr(mwoauth.get_current_user(False)) + "<br>" + \
-           "<a href=login>login</a> / <a href=logout>logout</a>"
+    greeting = app.config['GREETING']
+    username = flask.session.get('username', None)
+    return flask.render_template(
+        'index.html', username=username, greeting=greeting)
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
-f.close()
+@app.route('/login')
+def login():
+    """Initiate an OAuth login.
+	
+    Call the MediaWiki server to get request secrets and then redirect the
+    user to the MediaWiki server to sign the request.
+    """
+    consumer_token = mwoauth.ConsumerToken(
+        app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+    try:
+        redirect, request_token = mwoauth.initiate(
+            app.config['OAUTH_MWURI'], consumer_token)
+    except Exception:
+        app.logger.exception('mwoauth.initiate failed')
+        return flask.redirect(flask.url_for('index'))
+    else:
+        flask.session['request_token'] = dict(zip(
+            request_token._fields, request_token))
+        return flask.redirect(redirect)
+
+
+@app.route('/oauth-callback')
+def oauth_callback():
+    """OAuth handshake callback."""
+    if 'request_token' not in flask.session:
+        flask.flash(u'OAuth callback failed. Are cookies disabled?')
+        return flask.redirect(flask.url_for('index'))
+
+    consumer_token = mwoauth.ConsumerToken(
+        app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+
+    try:
+        access_token = mwoauth.complete(
+            app.config['OAUTH_MWURI'],
+            consumer_token,
+            mwoauth.RequestToken(**flask.session['request_token']),
+            flask.request.query_string)
+
+        identity = mwoauth.identify(
+            app.config['OAUTH_MWURI'], consumer_token, access_token)	
+    except Exception:
+        app.logger.exception('OAuth autnetication failed')
+	
+    else:
+        flask.session['access_token'] = dict(zip(
+            access_token._fields, access_token))
+        flask.session['username'] = identity['username']
+
+    return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    """Log the user out by clearing their session."""
+    flask.session.clear()
+    return flask.redirect(flask.url_for('index'))
